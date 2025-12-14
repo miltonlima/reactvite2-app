efeace42fb9bec60f28311fbc797d4cd98f7ff32
Milton Lima <milton.mlima@gmail.com>
2025-12-14 17:00:46 -0300

    Corrige importação do componente App6 e adiciona o componente App6 com navegação e contagem

---
+++ src/App6.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App2() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav className="menu">
+        <Link to="/">Home</Link>
+        <Link to="/page2">Page 2</Link>
+        <Link to="/page3">Page 3</Link>
+        <Link to="/page4">Page 4</Link>
+        <Link to="/page5">Page 5</Link>
+        <Link to="/page6">Page 6</Link>
+      </nav>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Voltei</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App2.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App2
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App2 from './App2.jsx'
 import App3 from './App3.jsx'
 import App4 from './App4.jsx'
 import App5 from './App5.jsx'
-import App5 from './App6.jsx'
+import App6 from './App6.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
