5070c63641526e029cd2dba3cc18da22f4f47311
Milton Lima <milton.mlima@gmail.com>
2025-12-15 23:06:28 -0300

    atualiza URL da API para buscar previsões do tempo

--- src/App6.jsx
+++ src/App6.jsx
@@ -14,7 +14,7 @@ function App6() {
     const controller = new AbortController()
     const fetchForecast = async () => {
       try {
-        const response = await fetch('https://localhost:7006/weatherforecast', {
+        const response = await fetch('https://aspnetcore2-api.onrender.com/weatherforecast', {
           signal: controller.signal,
         })
         if (!response.ok) {
